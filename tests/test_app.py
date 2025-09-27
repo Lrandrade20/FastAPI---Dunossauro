f97eee8ef49b8a8801907f4781b8208c51b846c9
Lucas Rocha <academicolucas93@gmail.com>
2025-09-27 12:45:55 -0300

    Feat: Push the created test structure

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,24 +12,3 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'Olá Mundo!'}
-
-
-"""
-
-* from http import HTTPStatus *: Importação da biblioteca nativa do python que abstrai os códigos de resposta e nos 
-apresenta o status.
-
-* def test_root_deve_retornar_ok_e_ola_mundo() *: No nome da função, escrevemos geralmente o que o teste de fato faz. 
-Aqui estamos dizendo que root deve retornar o status OK e a mensagem "olá mundo". Root é o nome dado a raiz da URL. 
-O caminho /, que colocamos na definição do @app.get('/'). OK é o status que diz que a requisição aconteceu com 
-sucesso no protocolo HTTP.
-
-* client = TestClient(app) * Aqui criamos o cliente de teste do nosso app
-
-* assert response.status_code == HTTPStatus.OK *: Aqui fazemos a validação do código de resposta, para saber se a 
-resposta é referente ao código 200, que significa OK
-
-* assert response.json() == {'message': 'Olá Mundo!'} *: No final, validamos se o dicionário que enviamos na função 
-é o mesmo que recebemos quando fizemos a requisição.
-
-"""
