8d3d63379fe2a76cc651cc8f6306f1cfc846a515
Lucas Rocha <academicolucas93@gmail.com>
2025-10-05 22:59:45 -0300

    Feat: Create schema.py initial structure & example route for learning on app.py

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=200, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+# Área dos exemplos
+@app.get('/ex1', response_class=HTMLResponse)
+def read_root_ex1():
+    return """
+    <html>
+      <head>
+        <title> Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html> """
