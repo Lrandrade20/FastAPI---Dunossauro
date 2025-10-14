c2e140b245a638168413fe0acb3dea10c11e08da
Lucas Rocha <academicolucas93@gmail.com>
2025-10-14 19:44:48 -0300

    Create test to endpoint 'ex1'

--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,13 @@ def test_root_deve_retornar_ok_e_ola_mundo():
 
     assert response.status_code == HTTPStatus.OK  # Assert
     assert response.json() == {'message': 'Olá Mundo!'}  # Assert
+
+
+def test_ex1_captura_do_response_do_html():
+    client = TestClient(app)  # Arrange
+
+    response = client.get('/ex1')  # Act
+
+    assert response.status_code == HTTPStatus.OK  # Assert
+    assert '<h1> Olá Mundo </h1>' in response.text
+    # print(response.text) 
